b47bd5628e2e76f20e7de595deb6cecb9c9f3b59
Antsax <antwan.hommy@gmail.com>
2018-02-12 14:41:47 +0200

    Osa3

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ let persons = [
 ]
 
 const formatPerson = (person) => {
-  const formattedPerson = {...person._doc, id: person._id}
+  const formattedPerson = { ...person._doc, id: person._id }
   delete formattedPerson._id
   delete formattedPerson.__v
   return formattedPerson
